3bb289a095525bfcf19c5e83eb591d912eed2d08
Marcin Gackowski <marcinga4@hotmail.com>
2020-03-20 13:06:15 +0100

    Change cursor color

--- CADence/cadStructures.cpp
+++ CADence/cadStructures.cpp
@@ -167,7 +167,47 @@ void Torus::UpdateObject()
 
 void SpawnMarker::RenderObject(std::unique_ptr<RenderData>& renderData, bool selected)
 {
-	this->RenderCoordinates(renderData, selected);
+	//Update content to fill constant buffer
+	D3D11_MAPPED_SUBRESOURCE res;
+	XMMATRIX mvp = m_transform.GetModelMatrix() * renderData->m_camera->GetViewProjectionMatrix();
+	//Set constant buffer
+	auto hres = renderData->m_device.context()->Map((renderData->m_cbMVP.get()), 0, D3D11_MAP_WRITE_DISCARD, 0, &res);
+	memcpy(res.pData, &mvp, sizeof(XMMATRIX));
+	renderData->m_device.context()->Unmap(renderData->m_cbMVP.get(), 0);
+	ID3D11Buffer* cbs[] = { renderData->m_cbMVP.get() };
+	renderData->m_device.context()->VSSetConstantBuffers(0, 1, cbs);
+
+	std::vector<VertexPositionColor> vertices{
+		{{-3.0f,0.0f,0.0f},{0.7f,0.7f,0.7f}},
+		{{3.0f,0.0f,0.0f} ,{0.7f,0.7f,0.7f}},
+		{{0.0f,-3.0f,0.0f},{0.7f,0.7f,0.7f}},
+		{{0.0f,3.0f,0.0f}, {0.7f,0.7f,0.7f}},
+		{{0.0f,0.0f,-3.0f},{0.7f,0.7f,0.7f}},
+		{{0.0f,0.0f,3.0f}, {0.7f,0.7f,0.7f}}
+	};
+
+	std::vector<unsigned short> indices{
+	0,1,
+	2,3,
+	4,5
+	};
+
+	renderData->m_device.context()->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_LINELIST);
+
+	// Update Vertex and index buffers
+	renderData->m_vertexBuffer = (renderData->m_device.CreateVertexBuffer(vertices));
+	renderData->m_indexBuffer = (renderData->m_device.CreateIndexBuffer(indices));
+	ID3D11Buffer* vbs[] = { renderData->m_vertexBuffer.get() };
+
+	//Update strides and offets based on the vertex class
+	UINT strides[] = { sizeof(VertexPositionColor) };
+	UINT offsets[] = { 0 };
+
+	renderData->m_device.context()->IASetVertexBuffers(0, 1, vbs, strides, offsets);
+
+	// Watch out for meshes that cannot be covered by ushort
+	renderData->m_device.context()->IASetIndexBuffer(renderData->m_indexBuffer.get(), DXGI_FORMAT_R16_UINT, 0);
+	renderData->m_device.context()->DrawIndexed(6, 0, 0);
 }
 
 bool SpawnMarker::CreateParamsGui()
