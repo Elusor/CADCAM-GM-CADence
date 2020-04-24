7f4e17b3b477b98df17203ce814403ec4794d8eb
Marcin Gackowski <marcinga4@hotmail.com>
2020-04-24 16:38:21 +0200

    Add solver for Tridiagonal matrix
    https://en.wikibooks.org/wiki/Algorithm_Implementation/Linear_Algebra/Tridiagonal_matrix_algorithm

--- CADence/mathUtils.cpp
+++ CADence/mathUtils.cpp
@@ -26,6 +26,39 @@ DirectX::XMFLOAT3 WeightedXMFloat3Average(DirectX::XMFLOAT3 f1, DirectX::XMFLOAT
 	return XMF3SUM(XMFloat3TimesFloat(f1, t), XMFloat3TimesFloat(f2, 1.0f - t));
 }
 
+// https://en.wikibooks.org/wiki/Algorithm_Implementation/Linear_Algebra/Tridiagonal_matrix_algorithm
+//| b0 c0 0 || x0 |  |d0|
+//|a1 b1 c1 || x1 |= |d1|
+//|0  a2 b2 || x2 |  |d2|
+std::vector<float> SolveTridiagMatrix(std::vector<float> lowerDiag, std::vector<float> diag, std::vector<float> upperDiag, std::vector<float> vector)
+{
+	
+	std::vector<float> a = lowerDiag;
+	std::vector<float> b = diag;
+	std::vector<float> c = upperDiag;
+	std::vector<float> d = vector;
+	int n = d.size();
+
+		n--; // since we start from x0 (not x1)
+		c[0] /= b[0];
+		d[0] /= b[0];
+
+		for (int i = 1; i < n; i++) 
+		{
+			c[i] /= b[i] - a[i] * c[i - 1];
+			d[i] = (d[i] - a[i] * d[i - 1]) / (b[i] - a[i] * c[i - 1]);
+		}
+
+		d[n] = (d[n] - a[n] * d[n - 1]) / (b[n] - a[n] * c[n - 1]);
+
+		for (int i = n; i> 0; i--)
+		{
+			d[i] -= c[i] * d[i + 1];
+		}	
+
+	return d;
+}
+
 // t * p0 + (1-t) * p1
 Transform WeightedTransformAverage(Transform p0, Transform p1, float t)
 {
