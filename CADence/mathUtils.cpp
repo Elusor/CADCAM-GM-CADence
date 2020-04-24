00c1fc93f5b78364a4292c85c0930ec2eb466080
Marcin Gackowski <marcinga4@hotmail.com>
2020-04-24 16:45:56 +0200

    Reorder utils to be in alphabettical order

--- CADence/mathUtils.cpp
+++ CADence/mathUtils.cpp
@@ -1,5 +1,38 @@
 #include "mathUtils.h"
 
+// https://en.wikibooks.org/wiki/Algorithm_Implementation/Linear_Algebra/Tridiagonal_matrix_algorithm
+//| b0 c0 0 || x0 |  |d0|
+//|a1 b1 c1 || x1 |= |d1|
+//|0  a2 b2 || x2 |  |d2|
+std::vector<float> SolveTridiagMatrix(std::vector<float> lowerDiag, std::vector<float> diag, std::vector<float> upperDiag, std::vector<float> vector)
+{
+
+	std::vector<float> a = lowerDiag;
+	std::vector<float> b = diag;
+	std::vector<float> c = upperDiag;
+	std::vector<float> d = vector;
+	int n = d.size();
+
+	n--; // since we start from x0 (not x1)
+	c[0] /= b[0];
+	d[0] /= b[0];
+
+	for (int i = 1; i < n; i++)
+	{
+		c[i] /= b[i] - a[i] * c[i - 1];
+		d[i] = (d[i] - a[i] * d[i - 1]) / (b[i] - a[i] * c[i - 1]);
+	}
+
+	d[n] = (d[n] - a[n] * d[n - 1]) / (b[n] - a[n] * c[n - 1]);
+
+	for (int i = n; i > 0; i--)
+	{
+		d[i] -= c[i] * d[i + 1];
+	}
+
+	return d;
+}
+
 DirectX::XMFLOAT3 XMF3SUM(DirectX::XMFLOAT3 v1, DirectX::XMFLOAT3 v2)
 {
 	return DirectX::XMFLOAT3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
@@ -21,44 +54,6 @@ DirectX::XMFLOAT3 XMFloat3TimesFloat(DirectX::XMFLOAT3 vec, float val)
 	return res;
 }
 
-DirectX::XMFLOAT3 WeightedXMFloat3Average(DirectX::XMFLOAT3 f1, DirectX::XMFLOAT3 f2, float t)
-{
-	return XMF3SUM(XMFloat3TimesFloat(f1, t), XMFloat3TimesFloat(f2, 1.0f - t));
-}
-
-// https://en.wikibooks.org/wiki/Algorithm_Implementation/Linear_Algebra/Tridiagonal_matrix_algorithm
-//| b0 c0 0 || x0 |  |d0|
-//|a1 b1 c1 || x1 |= |d1|
-//|0  a2 b2 || x2 |  |d2|
-std::vector<float> SolveTridiagMatrix(std::vector<float> lowerDiag, std::vector<float> diag, std::vector<float> upperDiag, std::vector<float> vector)
-{
-	
-	std::vector<float> a = lowerDiag;
-	std::vector<float> b = diag;
-	std::vector<float> c = upperDiag;
-	std::vector<float> d = vector;
-	int n = d.size();
-
-		n--; // since we start from x0 (not x1)
-		c[0] /= b[0];
-		d[0] /= b[0];
-
-		for (int i = 1; i < n; i++) 
-		{
-			c[i] /= b[i] - a[i] * c[i - 1];
-			d[i] = (d[i] - a[i] * d[i - 1]) / (b[i] - a[i] * c[i - 1]);
-		}
-
-		d[n] = (d[n] - a[n] * d[n - 1]) / (b[n] - a[n] * c[n - 1]);
-
-		for (int i = n; i> 0; i--)
-		{
-			d[i] -= c[i] * d[i + 1];
-		}	
-
-	return d;
-}
-
 // t * p0 + (1-t) * p1
 Transform WeightedTransformAverage(Transform p0, Transform p1, float t)
 {
@@ -66,4 +61,9 @@ Transform WeightedTransformAverage(Transform p0, Transform p1, float t)
 		WeightedXMFloat3Average(p0.GetPosition(), p1.GetPosition(), t),
 		WeightedXMFloat3Average(p0.GetRotation(), p1.GetRotation(), t),
 		WeightedXMFloat3Average(p0.GetScale(), p1.GetScale(), t));
+}
+
+DirectX::XMFLOAT3 WeightedXMFloat3Average(DirectX::XMFLOAT3 f1, DirectX::XMFLOAT3 f2, float t)
+{
+	return XMF3SUM(XMFloat3TimesFloat(f1, t), XMFloat3TimesFloat(f2, 1.0f - t));
 }
--- CADence/mathUtils.h
+++ CADence/mathUtils.h
@@ -1,9 +1,9 @@
 #pragma once
 #include "Transform.h"
 #include <vector>
-Transform WeightedTransformAverage(Transform p0, Transform p1, float t);
+std::vector<float> SolveTridiagMatrix(std::vector<float> a, std::vector<float> b, std::vector<float> c, std::vector<float> d);
 DirectX::XMFLOAT3 XMF3SUM(DirectX::XMFLOAT3 v1, DirectX::XMFLOAT3 v2);
 DirectX::XMFLOAT3 XMF3SUB(DirectX::XMFLOAT3 v1, DirectX::XMFLOAT3 v2);
 DirectX::XMFLOAT3 XMFloat3TimesFloat(DirectX::XMFLOAT3 vec, float val);
+Transform WeightedTransformAverage(Transform p0, Transform p1, float t);
 DirectX::XMFLOAT3 WeightedXMFloat3Average(DirectX::XMFLOAT3 f1, DirectX::XMFLOAT3 f2, float t);
-std::vector<float> SolveTridiagMatrix(std::vector<float> a, std::vector<float> b, std::vector<float> c, std::vector<float> d);
