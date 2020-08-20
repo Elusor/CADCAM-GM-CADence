d8fc62fb42bb0c82a33b061951973cc680d6808d
Marcin Gackowski <marcinga4@hotmail.com>
2020-08-20 17:50:41 +0200

    tmp

--- CADence/CurveVisualizer.cpp
+++ CADence/CurveVisualizer.cpp
@@ -299,8 +299,8 @@ std::vector<DirectX::XMFLOAT3> CurveVisualizer::GetInterpolationCurvePoints(std:
 		XMFLOAT3 pos1 = interpolationPoints[1];
 
 		resultPos.push_back(pos0);
-		resultPos.push_back(F3Lerp(pos0, pos1, 1.f / 3.f));
-		resultPos.push_back(F3Lerp(pos0, pos1, 2.f / 3.f));
+		resultPos.push_back(F3BadLerp(pos0, pos1, 1.f / 3.f));
+		resultPos.push_back(F3BadLerp(pos0, pos1, 2.f / 3.f));
 		resultPos.push_back(pos1);
 	}
 
