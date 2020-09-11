5fefff4bd5a0184ca9d0936c8ff6e6005479f0f9
Marcin Gackowski <marcinga4@hotmail.com>
2020-09-11 11:47:48 +0200

    Fix references contd

--- CADence/BezierSurfaceC2.cpp
+++ CADence/BezierSurfaceC2.cpp
@@ -3,9 +3,17 @@
 BezierSurfaceC2::BezierSurfaceC2(std::vector<std::shared_ptr<Node>> patches, int wCount, int hCount, SurfaceWrapDirection wrapDirection): 
 	BezierSurfaceC0(patches, wCount, hCount, wrapDirection)
 {
-
+	for (int i = 0; i < patches.size(); i++)
+	{
+		patches[i]->m_object->RefUse();
+	}
 }
 
 BezierSurfaceC2::~BezierSurfaceC2()
 {
+	// unmark all patches and remove them
+	for (int i = 0; i < m_patches.size(); i++)
+	{
+		m_patches[i]->m_object->GetReferences().UnlinkAll();
+	}
 }
